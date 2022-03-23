3e2e9ac978374f512a007e2ce565cc1c58350ce6
Benjamin Rechsteiner <benjamin.rechsteiner@abacus.ch>
2022-03-23 07:53:51 +0100

    typo

--- sslvpn-route.sh
+++ sslvpn-route.sh
@@ -112,7 +112,7 @@ function error {
 
 function getIps {
 	if [[ ${dest} =~ ^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$ ]]; then
-		debug 'Detect an IP adress'
+		debug 'Detect an IP address'
 		ips+=(${dest})
 	elif [[ ${dest} =~ ^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$ ]]; then
 		debug 'Dectect a hostname'
